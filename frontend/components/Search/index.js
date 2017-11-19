b30aef793c5b0b8c7ee985d93f2d4191ba672546
Jessica7 <jessica.nascimento6@gmail.com>
2017-11-18 21:17:25 -0300

    feat(component): Created Header component

--- frontend/components/Search/index.js
+++ frontend/components/Search/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import Input from '../Input';
+import Icon from '../Icon';
 
 let Search = ({ handleSubmit, onChange }) => (
   <form onChange={handleSubmit(onChange)}>
+    <span className="icon-search">
+      <Icon type={'fa fa-search'}
+            size={'small'}
+            color={'light-grey'} />
+    </span>
     <Field
       type="text"
       name="search"
-      label="Search"
       component={Input}
+      className="input-search"
       placeholder="search" />
   </form>
 );
