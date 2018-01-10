fc9416ae47487b373624334f14d1f463f258492f
Jessica7 <jessica.nascimento6@gmail.com>
2018-01-10 12:16:17 -0300

    style(select): Added placeholder, label and color error message validation on the select

--- frontend/components/Select/index.js
+++ frontend/components/Select/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactSelect from 'react-select';
 
-const Select = ({ multi, input, onBlur, options, meta: { error, touched } }) => {
+const Select = ({ multi, input, onBlur, options, placeholder, label, meta: { error, touched } }) => {
   return (
     <div className="wrapper-select">
+      {label ? <label htmlFor={input.name}>{label}</label> : null}
       <ReactSelect
         multi={multi}
         {...input}
         value={input.value || []}
         options={options}
+        placeholder={placeholder}
         onBlur={() => input.onBlur([...input.value])} />
-      {touched && error}
+        <span className="error">{touched && error}</span>
     </div>
   )
 };
